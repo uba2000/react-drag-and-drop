b542abb3d2cef5c9f9090b36fb5022a8764a0b88
uba2000 <ubanoel2000@gmail.com>
2022-03-08 13:11:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,77 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import './App.css';
 
+const finalSpaceCharacters = [
+  {
+    id: 'bulbasaur',
+    name: 'Bulbasaur',
+    thumb: '/images/bulbasaur.jpg'
+  },
+  {
+    id: 'charmander',
+    name: 'Charmander',
+    thumb: '/images/charmander.jpg'
+  },
+  {
+    id: 'caterpie',
+    name: 'Caterpie',
+    thumb: '/images/caterpie.jpg'
+  },
+  {
+    id: 'wartortle',
+    name: 'Wartortle',
+    thumb: '/images/wartortle.jpg'
+  },
+  {
+    id: 'charizard',
+    name: 'Charizard',
+    thumb: '/images/charizard.jpg'
+  }
+]
+
 function App() {
+  const [characters, updateCharacters] = useState(finalSpaceCharacters);
+
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+
+    const items = Array.from(characters);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    updateCharacters(items);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Pokemon Characters</h1>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="characters">
+            {(provided) => (
+              <ul className="characters" {...provided.droppableProps} ref={provided.innerRef}>
+                {characters.map(({ id, name, thumb }, index) => {
+                  return (
+                    <Draggable key={id} draggableId={id} index={index}>
+                      {(provided) => (
+                        <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                          <div className="characters-thumb">
+                            <img src={thumb} alt={`${name} Thumb`} />
+                          </div>
+                          <p>
+                            {name}
+                          </p>
+                        </li>
+                      )}
+                    </Draggable>
+                  );
+                })}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </DragDropContext>
       </header>
     </div>
   );
